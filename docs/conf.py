ea66e8387fdbe51dea02f906ea128b5d0286e4b6
Adam Fast <adam@adamfast.com>
2017-08-17 10:44:03 -0700

    Those were required. Oops.

--- docs/conf.py
+++ docs/conf.py
@@ -184,7 +184,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index',
+  ('index', 'beekeeper.tex', u'BeeKeeper Documentation',
    u'Russell Keith-Magee', 'manual'),
 ]
 
